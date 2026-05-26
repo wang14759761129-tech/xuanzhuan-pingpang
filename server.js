e12bf9ae0638c41661c12fa650b0da4ba13a04c3
wangchenxiang <wang14759761129@gmail.com>
2026-05-26 16:10:09 +0800

    🏓 v3.1 增强版: 支付方式+优惠券系统+增强结算

---
+++ server.js
@@ -0,0 +1,14 @@
+﻿const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const ROOT = 'C:/Users/wang/OneDrive/Desktop/pingpong_project';
+const MIME = {'.html':'text/html; charset=utf-8','.css':'text/css','.js':'application/javascript','.json':'application/json','.png':'image/png','.jpg':'image/jpeg'};
+const server = http.createServer((req, res) => {
+  let fp = path.join(ROOT, req.url === '/' ? 'pro.html' : req.url.replace(/^\//, ''));
+  const ext = path.extname(fp);
+  fs.readFile(fp, (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found'); }
+    else { res.writeHead(200, {'Content-Type': MIME[ext] || 'application/octet-stream'}); res.end(data); }
+  });
+});
+server.listen(8765, '127.0.0.1', () => console.log('Server ready'));
